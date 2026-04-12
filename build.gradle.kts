d3589d9a1fa49b620eb0c3586cfbab6028f09c0a
oimy <oimqop@gmail.com>
2026-04-12 14:37:14 +0900

    add logback

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
 	// spring
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 
 	// database
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
